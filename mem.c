7de401fff04de77e322dd74d09c42cac3c0ee682
Jiada <toendex@gmail.com>
2014-09-18 12:36:30 -0500

    a

--- mem.c
+++ mem.c
@@ -12,13 +12,79 @@
 #include <pthread.h>
 #include <string.h>
 
-#define ITERNUM 100000000L
-#define NUMOFNODE 1000000
+#define BLOCKSIZE 1
+#define BUFFERSIZE 100000000L
+#define ITERNUM 1000000000L
+#define RANDNUM 1000
+#define NUMOFTHREAD 1
+#define TESTTYPE 1
 
-struct node {
-    char a[256];
-    struct node *next;
+struct ParamForMemoryTest
+{
+    char *buffer1;
+    char *buffer2;
+    char *block;
+    long bufferSize;
+    long blockSize;
+    long iterNum;
+    int *rands;
 };
+
+void * calSeqRW(void *param)
+{
+    char *buffer1=((struct ParamForMemoryTest *)param)->buffer1;
+    char *buffer2=((struct ParamForMemoryTest *)param)->buffer2;
+    char *block=((struct ParamForMemoryTest *)param)->block;
+    long blockNum=BUFFERSIZE/BLOCKSIZE;
+    long loops=ITERNUM;
+    for (long i=0; i<loops; i++) {
+        memcpy(buffer1+BLOCKSIZE*(i%blockNum),buffer2+BLOCKSIZE*(i%blockNum),BLOCKSIZE);
+    }
+    pthread_exit(NULL);
+}
+
+void * calRanRW(void *param)
+{
+    char *buffer1=((struct ParamForMemoryTest *)param)->buffer1;
+    char *buffer2=((struct ParamForMemoryTest *)param)->buffer2;
+    char *block=((struct ParamForMemoryTest *)param)->block;
+    int *rand=((struct ParamForMemoryTest *)param)->rands;
+    long blockNum=BUFFERSIZE/BLOCKSIZE;
+    long loops=ITERNUM/2;
+    for (long i=0; i<loops; i++) {
+        memcpy(buffer1+BLOCKSIZE*((i+rand[i%RANDNUM])%blockNum),buffer2+BLOCKSIZE*((i+rand[i%RANDNUM])%blockNum),BLOCKSIZE);
+//        printf("%d\n",rand[i%RANDNUM]%blockNum);
+    }
+    pthread_exit(NULL);
+}
+
+void * calSeqLat(void *param)
+{
+    char *buffer1=((struct ParamForMemoryTest *)param)->buffer1;
+    char *buffer2=((struct ParamForMemoryTest *)param)->buffer2;
+    char *block=((struct ParamForMemoryTest *)param)->block;
+    long blockNum=BUFFERSIZE;
+    long loops=ITERNUM/2;
+    for (long i=0; i<loops; i++) {
+        memcpy(buffer1+(i%blockNum),buffer2+(i%blockNum),BLOCKSIZE);
+    }
+    pthread_exit(NULL);
+}
+
+void * calRanLat(void *param)
+{
+    char *buffer1=((struct ParamForMemoryTest *)param)->buffer1;
+    char *buffer2=((struct ParamForMemoryTest *)param)->buffer2;
+    char *block=((struct ParamForMemoryTest *)param)->block;
+    int *rand=((struct ParamForMemoryTest *)param)->rands;
+    long blockNum=BUFFERSIZE/BLOCKSIZE;
+    long loops=ITERNUM/2;
+    for (long i=0; i<loops; i++) {
+        memcpy(buffer1+BLOCKSIZE*((i+rand[i%RANDNUM])%blockNum),buffer2+BLOCKSIZE*((i+rand[i%RANDNUM])%blockNum),BLOCKSIZE);
+        //        printf("%d\n",rand[i%RANDNUM]%blockNum);
+    }
+    pthread_exit(NULL);
+}
 
 double nowTimeInSec() {
     struct timeval t;
@@ -29,27 +95,57 @@ double nowTimeInSec() {
 int main(int argc, const char * argv[])
 {
     double t;
+    int testType;
+    int numOfThread;
+    char *buffer1;
+    char *buffer2;
+    char *block;
+    int rands[RANDNUM*NUMOFTHREAD];
+    pthread_attr_t attr;
+    pthread_t *threads;
     
-    struct node *nw;
-    struct node *nodes=(struct node*)malloc(sizeof(struct node)*NUMOFNODE);
-    long wLoops=((NUMOFNODE+1)>ITERNUM)?(NUMOFNODE+1):ITERNUM;
-    nw=nodes;
+    testType=TESTTYPE;
+    numOfThread=NUMOFTHREAD;
+    threads=(pthread_t*)malloc(sizeof(pthread_t)*numOfThread);
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+    struct ParamForMemoryTest *paramForMemoryTest;
+    paramForMemoryTest=(struct ParamForMemoryTest *)malloc(sizeof(struct ParamForMemoryTest)*numOfThread);
+    for (int i=0; i<numOfThread; i++) {
+        paramForMemoryTest[i].buffer1=(char*)malloc(BUFFERSIZE);
+        paramForMemoryTest[i].buffer2=(char*)malloc(BUFFERSIZE);
+        paramForMemoryTest[i].block=(char*)malloc(BLOCKSIZE);
+        paramForMemoryTest[i].rands=rands+i*RANDNUM;
+    }
+    srand(time(NULL));
+    for (int i=0; i<RANDNUM*NUMOFTHREAD; i++) {
+        rands[i]=rand();
+    }
+    
     t=nowTimeInSec();
-    for (long i=0; i<wLoops; i++) {
-        nw->next=nodes+(i%NUMOFNODE);
-        nw=nw->next;
+    for (int i=0; i<numOfThread; i++) {
+        if (testType==0) {
+            pthread_create(threads+i, &attr, calSeqRW, &paramForMemoryTest[i]);
+        }
+        if (testType==1) {
+            pthread_create(threads+i, &attr, calRanRW, &paramForMemoryTest[i]);
+        }
+    }
+    for (int i=0; i<numOfThread; i++) {
+        pthread_join(threads[i], NULL);
     }
     t=nowTimeInSec()-t;
-    printf("Write Latency: Run %.2lfs,with %.4lfMT/s, %.4lfns latency\n",t,wLoops/t/1.e6,t*1.e9/wLoops);
     
-    nw=nodes;
-    t=nowTimeInSec();
-    for (long i=0; i<ITERNUM; i++) {
-        nw=nw->next;
+    printf("Copy: Run %.2lfs, block size %d, with %.4lfMT/s, %.4lfGB/s, %.4lfns latency\n",t,BLOCKSIZE,ITERNUM*BLOCKSIZE*numOfThread/t/1.e6,ITERNUM*BLOCKSIZE*numOfThread/t/1.e9,t*1.e9/ITERNUM/numOfThread);
+    
+    for (int i=0; i<numOfThread; i++) {
+        free(paramForMemoryTest[i].buffer1);
+        free(paramForMemoryTest[i].buffer2);
+        free(paramForMemoryTest[i].block);
     }
-    t=nowTimeInSec()-t;
-    printf("%i\n",nw->a[0]);
-    printf("Read Latency: Run %.2lfs,with %.4lfMT/s, %.4lfns latency\n",t,ITERNUM/t/1.e6,t*1.e9/ITERNUM);
+    pthread_attr_destroy(&attr);
+    free(threads);
     
     return 0;
 }
